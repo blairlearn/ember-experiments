b621189d595f01dfa13197805933657b0ecf388b
Blair Learn <blairlearn@users.noreply.github.com>
2014-05-09 18:09:50 +0000

    git-svn-id: svn://localhost/DevelopersFolders/BlairL/Ember@10 42471d10-2665-b34d-a512-8166a232a7fd

---
+++ O'Reilly/server/server.js
@@ -0,0 +1,48 @@
+// DEPENDENCIES
+// ============
+var express = require("express"),
+    http = require("http"),
+    httpProxy = require("http-proxy"), //added to proxy service requests
+    port = (process.env.PORT || 8002),
+    server = module.exports = express();
+
+//Proxy options below are to proxy service requests
+var proxy = new httpProxy.RoutingProxy();
+
+
+// SERVER CONFIGURATION
+// ====================
+server.configure(function () {
+
+    server.use(express["static"](__dirname + "/../public"));
+
+    server.use(express.errorHandler({
+
+        dumpExceptions:true,
+
+        showStack:true
+
+    }));
+
+
+    server.use(server.router);
+});
+
+// SERVER
+// ======
+
+//Added to proxy request
+server.all('/TermDictionary.svc/*', function(req, res) {
+    var prxReq = proxy.proxyRequest(req, res, {
+        host: 'www.cancer.gov',
+        port: 80,
+        changeOrigin: true
+    });
+
+    return prxReq;
+});
+
+// Start Node.js Server
+http.createServer(server).listen(port);
+
+console.log('Welcome to MRB-Lite!\n\nPlease go to http://localhost:' + port + ' to start using Marionette, Require.js and Backbone.js');
